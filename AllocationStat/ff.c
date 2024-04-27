d47e94a603c0accd614cc3377393a4b1336f16e3
dEIOnTomsoN <125672061+dEIOnTomsoN@users.noreply.github.com>
2024-04-27 20:38:18 +0530

    Add files via upload

---
+++ AllocationStat/ff.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+void firstFit(int blockSize[],int m,int processSize[],int n)
+{
+   int allocated[10];
+
+   for(int i = 0; i <n;i++)
+   {
+      allocated[i] = -1;
+   }
+
+   for(int i=0;i<n;i++)
+   {
+      for(int j = 0; j <m; j++)
+      {
+         if(blockSize[j] >= processSize[i])
+         {
+            allocated[i] = j;
+            printf("%d - %d = ",blockSize[j],processSize[i]);
+            blockSize[j] -= processSize[i];
+            printf("%d\n",blockSize[j]);
+            break;
+
+         }
+      }
+   }
+
+   printf("\nProcess\t\tdataSize\t\tallocated\n");
+   for(int i = 0; i <n;i++)
+   {
+      printf("%d\t\t%d\t\t\t",i+1,processSize[i]);
+      if(allocated[i]==-1)
+      {
+         printf("Not allocated\n");
+      }
+      else{
+          printf("%d\n",allocated[i]+1);
+      }
+   }
+}
+
+
+int main()
+{
+    int m,n,blockSize[10],ProcessSize[10];
+    printf("Enter the number of Blocks: ");
+    scanf("%d",&m);
+    printf("Enter the Block sizes of each: ");
+    for(int i=0; i<m; i++)
+    {
+      scanf("%d",&blockSize[i]);
+    }
+    printf("Enter the number of processes: ");
+    scanf("%d",&n);
+    printf("Enter the Process size of each: ");
+    for(int i=0; i<n; i++)
+    {
+      scanf("%d",&ProcessSize[i]);
+    }
+    firstFit(blockSize,m,ProcessSize,n);
+
+}
